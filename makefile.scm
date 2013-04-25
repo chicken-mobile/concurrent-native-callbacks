4a378380a68a0750c3b151986f82c00683bc4ff4
felix <felix@call-with-current-continuation.org>
2013-04-25 14:15:52 +0200

    added necessary egg files and a more or less useless README.

--- makefile.scm
+++ makefile.scm
@@ -8,35 +8,37 @@
 	   "cncb-module.scm"
 	   "twiddle.c"
 	   "concurrent-native-callbacks-compile-time.import.so")
-	  (run (csc -sJ -O2 cncb-module.scm
-		    -C -g -k			;XXX
+	  (run (csc -sJ -O3 cncb-module.scm
+		    ;-C -g -k			;XXX
 		    -C -Wno-unused-result
 		    -o concurrent-native-callbacks.so)) )
 	 ("concurrent-native-callbacks.import.so"
 	  ("concurrent-native-callbacks.so")
-	  (run (csc -s -O2 -d0 concurrent-native-callbacks.import.scm)))
+	  (run (csc -s -O3 -d0 concurrent-native-callbacks.import.scm)))
 	 ("concurrent-native-callbacks-compile-time.so"
 	  ("cncb-compile-time.scm" 
 	   "cncb-compile-time-module.scm")
-	  (run (csc -sJ -O2 cncb-compile-time-module.scm
+	  (run (csc -sJ -O3 cncb-compile-time-module.scm
 		    -o concurrent-native-callbacks-compile-time.so)))
 	 ("concurrent-native-callbacks-compile-time.import.so"
 	  ("concurrent-native-callbacks-compile-time.so")
-	  (run (csc -s -O2 -d0
+	  (run (csc -s -O3 -d0
 		    concurrent-native-callbacks-compile-time.import.scm)))
 	 ("test1" 
 	  ("simple-test.c"
 	   "cncb-simple-test.scm"
 	   "concurrent-native-callbacks.import.so")
-	  (run (csc -k -C -g simple-test.c -o test1
-		    -C -Wno-unused-result ;-C "-O0"
+	  (run (csc ;-k -C -g
+		    simple-test.c -o test1
+		    -C -Wno-unused-result
 		    cncb-simple-test.scm -e
 		    -L$HOME/lib -lchicken -lpthread)))
 	 ("test2" ("hammering.c" 
 		   "cncb-simple-test.scm"
 		   "concurrent-native-callbacks.import.so")
-	  (run (csc -k -C -g hammering.c -o test2
-		    -C -Wno-unused-result ;-C -O0
+	  (run (csc ;-k -C -g
+		    hammering.c -o test2
+		    -C -Wno-unused-result
 		    cncb-simple-test.scm -e
 		    -L$HOME/lib -lchicken -lpthread))))
     args))
