a57972a69d967a0d43d5f326b02a592115481383
Evan Hanson <evhan@foldling.org>
2014-04-20 19:42:25 +1200

    remove -Wno-unused-result from test compilation commands

--- tests/run.scm
+++ tests/run.scm
@@ -5,14 +5,12 @@
 	 "cncb-simple-test.scm")
 	(run (csc			;-k -C -g
 	      simple-test.c -o test1
-	      -C -Wno-unused-result
 	      cncb-simple-test.scm -e
 	      -lpthread)))
        ("test2" ("hammering.c" 
 		 "cncb-simple-test.scm")
 	(run (csc			;-k -C -g
 	      hammering.c -o test2
-	      -C -Wno-unused-result
 	      cncb-simple-test.scm -e
 	      -lpthread))))
   '("test1" "test2"))
