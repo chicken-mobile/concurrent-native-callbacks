5b922d0553eef1e0b883f8169765b9082cc59b12
felix <felix@call-with-current-continuation.org>
2019-06-21 11:00:06 +0200

    tests: drop debugging flags, add -L before specifying pthreads library option

--- tests/run.scm
+++ tests/run.scm
@@ -3,16 +3,16 @@
 (make (("test1" 
 	("simple-test.c"
 	 "cncb-simple-test.scm")
-	(run (csc			-k -C -g
+	(run (csc
 	      simple-test.c -o test1
 	      cncb-simple-test.scm -e
-	      -lpthread)))
+	      -L -lpthread)))
        ("test2" ("hammering.c" 
 		 "cncb-simple-test.scm")
-	(run (csc			-k -C -g
+	(run (csc
 	      hammering.c -o test2
 	      cncb-simple-test.scm -e
-	      -lpthread))))
+	      -L -lpthread))))
   '("test1" "test2"))
 
 (run (./test1))
